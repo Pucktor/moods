06d2780536edf1431773e92a49969b0690a67edd
Paul Heilweck <paul.heilweck@edu.escpeurope.eu>
2020-02-24 17:16:19 +0100

    Basic seed added

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_132107) do
+ActiveRecord::Schema.define(version: 2020_02_24_155028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.integer "spotify_genre_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
