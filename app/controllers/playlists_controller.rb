655feab505192afe2e223b1de18e4959b1836941
Dimby Rakotomalala <dimby777@gmail.com>
2020-02-24 16:22:05 +0100

    Ajout de l'index, new/create de playlists

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,26 @@
 class PlaylistsController < ApplicationController
+  def index
+    @playlists = Playlist.where(user_id: current_user.id)
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+    @playlist.user = current_user
+    @playlist.save
+    if @playlist.save
+      redirect_to playlists_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :acousticness, :danceability, :energy, :valence, :popularity)
+  end
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,2 +1,6 @@
 class Playlist < ApplicationRecord
+  belongs_to :user
+  has_many :genres, through: :playlist_genres
+  has_many :tracks, through: :playlist_tracks
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
 end
