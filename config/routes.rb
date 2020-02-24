9a1e6de4d8a8007d3a42f6b278286f2b98e8f672
patrice jaszczynski <ppastg92@free.fr>
2020-02-24 14:07:12 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#home'
-  resources :playlits
+  resources :playlists
 end
