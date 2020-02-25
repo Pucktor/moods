685f4cfb327427bab50ec106d184f8317d2abd91
Julien Pierlot <pierlot.julien@gmail.com>
2020-02-25 18:20:48 +0100

    fix typo

--- app/services/create_spotify_playlist.rb
+++ app/services/create_spotify_playlist.rb
@@ -1,7 +1,7 @@
 class CreateSpotifyPlaylist
-  def self.call(playlist, recommentations_tracks, spotify_user)
+  def self.call(playlist, recommendations_tracks, spotify_user)
     spotify_playlist = spotify_user.create_playlist!(playlist.name)
-    spotify_playlist.add_tracks!(recommentations_tracks)
+    spotify_playlist.add_tracks!(recommendations_tracks)
     playlist.update(spotify_id: spotify_playlist.id)
   end
 end
