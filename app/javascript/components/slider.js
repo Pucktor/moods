934a04c938c2ba8dee2e86489c0281a6ac9a4bb7
patrice jaszczynski <ppastg92@free.fr>
2020-02-27 10:40:03 +0100

    slider color dynamic with javascript

--- app/javascript/components/slider.js
+++ app/javascript/components/slider.js
@@ -4,19 +4,24 @@ const sliderRange = () => {
 const sliders = document.querySelectorAll(".slider");
 
 sliders.forEach((slider) => {
-    const valeur = slider.value;
-    slider.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(10,207,213,1) ' + valeur*100 + '%, #fff ' + valeur*100 + '%, white 100%)';
 
+  const valeur = slider.value;
+  const name = slider.name;
+
+  if (name == "playlist[popularity]") {
+    slider.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(10,207,213,1) ' + valeur + '%, #fff ' + valeur + '%, white 100%)';
+    slider.oninput = function() {
+    this.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(10,207,213,1) ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
+    };
+  }
+  else {
+    slider.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(10,207,213,1) ' + valeur*100 + '%, #fff ' + valeur*100 + '%, white 100%)';
     slider.oninput = function() {
     this.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(10,207,213,1) ' + this.value*100 + '%, #fff ' + this.value*100 + '%, white 100%)'
-    };
+  };
+  }
+
 });
 }
-// const sliderRange = () => {
-// document.getElementById("myinput").oninput = function() {
-//   this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + this.value*100 + '%, #fff ' + this.value*100 + '%, white 100%)'
-//   };
-// }
-
 
 export { sliderRange };
