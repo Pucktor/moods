399f2f3bd0980c252f72d065056754801b947bf0
patrice jaszczynski <ppastg92@free.fr>
2020-02-28 12:07:42 +0100

    Add title for playlist page

--- app/javascript/components/slider.js
+++ app/javascript/components/slider.js
@@ -9,18 +9,17 @@ sliders.forEach((slider) => {
   const name = slider.name;
 
   if (name == "playlist[popularity]") {
-    slider.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(10,207,213,1) ' + valeur + '%, #fff ' + valeur + '%, transparent 100%)';
+    slider.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(137, 255, 253, 0.9) ' + valeur + '%, #fff ' + valeur + '%, transparent 100%)';
     slider.oninput = function() {
-    this.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(10,207,213,1) ' + this.value + '%, #fff ' + this.value + '%, transparent 100%)'
+    this.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(137, 255, 253, 0.9) ' + this.value + '%, #fff ' + this.value + '%, transparent 100%)'
     };
   }
   else {
-    slider.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(10,207,213,1) ' + valeur*100 + '%, #fff ' + valeur*100 + '%, transparent 100%)';
+    slider.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(137, 255, 253, 0.9) ' + valeur*100 + '%, #fff ' + valeur*100 + '%, transparent 100%)';
     slider.oninput = function() {
-    this.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(10,207,213,1) ' + this.value*100 + '%, #fff ' + this.value*100 + '%, transparent 100%)'
+    this.style.background = 'linear-gradient(to right, rgba(9,142,237,1) 0%, rgba(137, 255, 253, 0.9) ' + this.value*100 + '%, #fff ' + this.value*100 + '%, transparent 100%)'
   };
   }
-
 });
 }
 
