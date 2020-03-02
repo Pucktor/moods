c90370778675dbadb9f4df4236bd6db2ef197031
Julien Pierlot <pierlot.julien@gmail.com>
2020-03-02 12:42:21 +0100

    fix playlist url

--- app/services/create_spotify_playlist.rb
+++ app/services/create_spotify_playlist.rb
@@ -2,6 +2,6 @@ class CreateSpotifyPlaylist
   def self.call(playlist, recommendations_tracks, spotify_user)
     spotify_playlist = spotify_user.create_playlist!(playlist.name)
     spotify_playlist.add_tracks!(recommendations_tracks)
-    playlist.update(spotify_id: spotify_playlist.id, image_url: spotify_playlist.images.first["url"])
+    playlist.update(spotify_id: spotify_playlist.id)
   end
 end
