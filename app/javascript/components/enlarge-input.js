32bfabf8a3e84dfda40dad8c0cfd5ac0683d6460
Dimby Rakotomalala <dimby777@gmail.com>
2020-03-03 17:35:25 +0100

    edit ok

--- app/javascript/components/enlarge-input.js
+++ app/javascript/components/enlarge-input.js
@@ -9,6 +9,10 @@ const enlargeInput = () => {
   } else if (window.location.pathname.slice(-5) == '/edit') {
     const inputValue = document.querySelector("#playlist_name");
     inputValue.style.width = inputValue.value.length + "ch";
+    inputValue.addEventListener("input", (event) => {
+      var spaceCount = (inputValue.value.split(" ").length - 1);
+      event.currentTarget.style.width = 1 + event.currentTarget.value.length - 0.8 * spaceCount + "ch";
+    });
   }
 }
 
