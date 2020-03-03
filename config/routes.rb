0e6fdd8cd7f7fda893f02b089841831af74dd529
Dimby Rakotomalala <dimby777@gmail.com>
2020-03-03 15:35:40 +0100

    Correction form

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,12 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-
-
   resources :playlists do
     get '/refresh', to: 'playlists#refresh'
   end
 
-  resources :playlists, only: :show do
-    get '/refresh', to: 'playlists#refresh'
-  end
+  # resources :playlists, only: :show do
+  #   get '/refresh', to: 'playlists#refresh'
+  # end
 
 end
