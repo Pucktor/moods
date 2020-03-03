f285e487bc087bdc143d7265ffccec2f5853299d
Dimby Rakotomalala <dimby777@gmail.com>
2020-03-03 15:05:55 +0100

    Correction route

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
   root to: 'playlists#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  
-  resources :playlists, only: :show do
+
+
+
+  resources :playlists do
     get '/refresh', to: 'playlists#refresh'
   end
 
-  resources :playlists
+  resources :playlists, only: :show do
+    get '/refresh', to: 'playlists#refresh'
+  end
 
 end
