49bd131b73a009b8a5d377da5297868c8d481ad3
Julien Pierlot <pierlot.julien@gmail.com>
2020-03-05 12:33:35 +0100

    fix sign in

--- app/services/update_user_account_type.rb
+++ app/services/update_user_account_type.rb
@@ -3,7 +3,7 @@ class UpdateUserAccountType
     url = 'https://api.spotify.com/v1/me'
     headers = {
       content_type: 'application/x-www-form-urlencoded',
-      Authorization: "Bearer BQClj_k07CTG7xIgcBmK_q5UaXlIO-VIWPnvkcote1L05lYOgoUkJNhPaFbKoFWuTvWrhKtKDw-2e4aTxC7w9FWp_P1dgH94vpDgfc9oVaNnzW4VEaL54GOdIaiVbbmv1oFFh6QpREmhLpAoJyyxodizMhqlWwGxIJZOFY-YRZzMiT58iouzMihe4iBPQLKBD_KsGW7B5-in0acv1C5FK4ucFRP8Hq_Q1aQVDpNfaPR_cOzI51ElbgZIyJrJRx_KYA"
+      Authorization: "Bearer #{user.token}"
     }
     response = JSON.parse(RestClient.get(url, headers))
     if response['product'] == 'premium'
