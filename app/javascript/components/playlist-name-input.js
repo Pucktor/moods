fd03546155c824bdb3d773e49bba6019d2d7bd4b
patrice jaszczynski <ppastg92@free.fr>
2020-03-05 17:20:27 +0100

    Fix name missing on edit page

--- app/javascript/components/playlist-name-input.js
+++ app/javascript/components/playlist-name-input.js
@@ -2,7 +2,7 @@ const playlistNameInput = () => {
   const input = document.getElementById("playlist-name");
   const name = document.getElementById("playlist_name");
   const form = document.querySelector("form");
-
+  input.value = name.value;
   if (name) {
     form.addEventListener("submit", (event) => {
       name.value = input.value;
