ddc618c0bfd762a90a0dc9fadab031b74f00a1ef
patrice jaszczynski <ppastg92@free.fr>
2020-03-05 10:13:19 +0100

    Change enlarge input for playlist title

--- app/javascript/components/mood-color-picker.js
+++ app/javascript/components/mood-color-picker.js
@@ -10,7 +10,8 @@ const moodColorPicker = () => {
       const colorName = event.currentTarget.textContent;
       const colorSelected = document.getElementById('playlist_color');
       colorSelected.value = colorName;
-
+      const cadre = document.querySelector(".card.example");
+      cadre.style.background = colorName;
     });
   });
 };
