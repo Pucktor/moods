8f5c0ee072f2ecdd0e9b1aea4de862d846bfaaec
Dimby Rakotomalala <dimby777@gmail.com>
2020-03-05 10:53:50 +0100

    Create/edit : les éléments se mettent à jour en fonction des sélection. KO pour les genres

--- app/javascript/components/mood-color-picker.js
+++ app/javascript/components/mood-color-picker.js
@@ -11,6 +11,18 @@ const moodColorPicker = () => {
       const colorSelected = document.getElementById('playlist_color');
       colorSelected.value = colorName;
 
+      const card = document.querySelector(".card.example");
+      const acousticness = document.querySelector("div.vbar-div > div:nth-child(1) > div.filledvbar.example");
+      const danceability = document.querySelector("div.vbar-div > div:nth-child(2) > div.filledvbar.example");
+      const energy = document.querySelector("div.vbar-div > div:nth-child(3) > div.filledvbar.example");
+      const valence = document.querySelector("div.vbar-div > div:nth-child(4) > div.filledvbar.example");
+      const popularity = document.querySelector("div.vbar-div > div:nth-child(5) > div.filledvbar.example");
+      card.style.backgroundImage = event.currentTarget.style.backgroundImage;
+      acousticness.style.backgroundImage = event.currentTarget.style.backgroundImage;
+      danceability.style.backgroundImage = event.currentTarget.style.backgroundImage;
+      energy.style.backgroundImage = event.currentTarget.style.backgroundImage;
+      valence.style.backgroundImage = event.currentTarget.style.backgroundImage;
+      popularity.style.backgroundImage = event.currentTarget.style.backgroundImage;
     });
   });
 };
