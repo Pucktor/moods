87a1b4e7785b4d870e2f715d03b493cf79767953
Paul Heilweck <paul.heilweck@edu.escpeurope.eu>
2020-03-11 19:06:46 +0100

    continue responsive

--- app/javascript/components/mood-color-picker.js
+++ app/javascript/components/mood-color-picker.js
@@ -10,22 +10,32 @@ const moodColorPicker = () => {
       const colorName = event.currentTarget.textContent;
       const colorSelected = document.getElementById('playlist_color');
       colorSelected.value = colorName;
-      
+
       const card = document.querySelector(".card.example");
       const acousticness = document.querySelector("div.vbar-div > div:nth-child(1) > div.filledvbar.example");
       const danceability = document.querySelector("div.vbar-div > div:nth-child(2) > div.filledvbar.example");
       const energy = document.querySelector("div.vbar-div > div:nth-child(3) > div.filledvbar.example");
       const valence = document.querySelector("div.vbar-div > div:nth-child(4) > div.filledvbar.example");
       const popularity = document.querySelector("div.vbar-div > div:nth-child(5) > div.filledvbar.example");
-      
+      const sliderBgAcousticness = document.querySelector(".slider-bg.acousticness");
+      const sliderBgDanceability = document.querySelector(".slider-bg.danceability");
+      const sliderBgEnergy = document.querySelector(".slider-bg.energy");
+      const sliderBgValence = document.querySelector(".slider-bg.valence");
+      const sliderBgPopularity = document.querySelector(".slider-bg.popularity");
+
+      sliderBgAcousticness.style.backgroundImage = event.currentTarget.style.backgroundImage;
+      sliderBgDanceability.style.backgroundImage = event.currentTarget.style.backgroundImage;
+      sliderBgEnergy.style.backgroundImage = event.currentTarget.style.backgroundImage;
+      sliderBgValence.style.backgroundImage = event.currentTarget.style.backgroundImage;
+      sliderBgPopularity.style.backgroundImage = event.currentTarget.style.backgroundImage;
+
       card.style.backgroundImage = event.currentTarget.style.backgroundImage;
       acousticness.style.backgroundImage = event.currentTarget.style.backgroundImage;
       danceability.style.backgroundImage = event.currentTarget.style.backgroundImage;
       energy.style.backgroundImage = event.currentTarget.style.backgroundImage;
       valence.style.backgroundImage = event.currentTarget.style.backgroundImage;
       popularity.style.backgroundImage = event.currentTarget.style.backgroundImage;
-      // const cadre = document.querySelector(".card.example");
-      // cadre.style.background = colorName;
+
     });
   });
 };
